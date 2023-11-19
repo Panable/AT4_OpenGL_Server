7a84accbc792ffd0b553d0fc82fefbcfca5be3a9
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-11-20 04:14:57 +1100

    consitency

---
+++ build/CMakeFiles/ExampleChat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleChat.dir/example_chat.cpp.o"
+  "CMakeFiles/ExampleChat.dir/example_chat.cpp.o.d"
+  "ExampleChat"
+  "ExampleChat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ExampleChat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ExampleChat.
+# This may be replaced when dependencies are built.
